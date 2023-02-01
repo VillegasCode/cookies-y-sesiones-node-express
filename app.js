24b00c1b52e33b92bb8445dae331204f3d32ceeb
Carlos Villegas <carlos.villegasch@gmail.com>
2023-01-31 20:56:39 -0500

    Importando MySQL Session con una base de datos llamada prueba_session

--- app.js
+++ app.js
@@ -3,6 +3,33 @@ const session = require('express-session');
 
 const app = express();
 
+//Importando mySQL session
+const mySQLStore = require('express-mysql-session');
+const options = {
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: '',
+    database: 'prueba_session'
+}
+
+//Vamos a guardar las sesiones ejemplo simple
+// app.use(session({
+//     secret:'1234567',
+//     resave: true,
+//     saveUninitialized: true
+// }))
+
+
+app.get('/', (req, res)=>{
+    req.session.usuario = 'Juan Perez';
+    req.session.rol = 'Admin';
+    req.session.visitas = req.session.visitas ? ++req.session.visitas : 1;
+    console.log(req.session);
+    res.send(`El usuario <strong>${req.session.usuario}<strong>
+            con rol <strong>${req.session.rol}</strong> ha visitado esta página ${req.session.visitas} veces`)
+})
+
 app.listen(3000, (req, res)=>{
     console.log("SERVER UP!");
 });
